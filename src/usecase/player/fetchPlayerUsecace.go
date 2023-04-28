f56aa3370ab6f47cb20d0035160746f396710214
tomoya <tomoya.kamaji@gmail.com>
2023-04-29 00:04:10 +0900

    dtoにくるむように

--- src/usecase/player/fetchPlayerUsecace.go
+++ src/usecase/player/fetchPlayerUsecace.go
@@ -15,11 +15,32 @@ func NewFetchPlayerUsecase(playerRepository domain.PlayerRepository) *FetchPlaye
 		playerRepository: playerRepository,
 	}
 }
-func (u *FetchPlayerUsecase) Run(c context.Context, ID string) (*domain.Player, error) {
+
+type FetchPlayerUsecaseDTO struct {
+	ID            string
+	UniformNumber int64
+	Name          string
+	AtBats        int64
+	Hits          int64
+	Walks         int64
+	HomeRuns      int64
+	RunsBattedIn  int64
+}
+
+func (u *FetchPlayerUsecase) Run(c context.Context, ID string) (*FetchPlayerUsecaseDTO, error) {
 	var player *domain.Player
 	player, err := u.playerRepository.GetByID(c, domain.PlayerID(ID))
 	if err != nil {
 		return nil, err
 	}
-	return player, nil
+	return &FetchPlayerUsecaseDTO{
+		ID:            player.ID.String(),
+		UniformNumber: player.UniformNumber,
+		Name:          player.Name,
+		AtBats:        player.AtBats,
+		Hits:          player.Hits,
+		Walks:         player.Walks,
+		HomeRuns:      player.HomeRuns,
+		RunsBattedIn:  player.RunsBattedIn,
+	}, nil
 }

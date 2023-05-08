fd8d80a742db9811770203fdf32a5713d1b681cb
tomoya <tomoya.kamaji@gmail.com>
2023-05-08 22:44:54 +0900

    クロールの処理を作成

--- src/api/api.go
+++ src/api/api.go
@@ -2,11 +2,18 @@ package api
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	pb "github.com/tomoya_kamaji/go-pkg/grpc"
 	"github.com/tomoya_kamaji/go-pkg/src/adapter/gorm"
+	domain "github.com/tomoya_kamaji/go-pkg/src/domain/player"
 	"github.com/tomoya_kamaji/go-pkg/src/infrastructure/repositoryImpl"
 	usecase "github.com/tomoya_kamaji/go-pkg/src/usecase/player"
+	"github.com/tomoya_kamaji/go-pkg/src/util/cast"
 	"github.com/tomoya_kamaji/go-pkg/src/util/logging"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
@@ -67,44 +74,57 @@ func (s *baseBallApiServer) CreatePlayer(ctx context.Context, in *pb.CreatePlaye
 	}}, nil
 }
 
+// 以下のスクレイピングコードは、別のパッケージに切り出すべき
+var teamSiteUIDMap = map[string]string{
+	"巨人":     "g",
+	"阪神":     "t",
+	"広島":     "c",
+	"ヤクルト":   "s",
+	"中日":     "d",
+	"DeNA":   "yb",
+	"西武":     "l",
+	"ソフトバンク": "h",
+	"楽天":     "e",
+	"ロッテ":    "m",
+	"オリックス":  "bs",
+	"日本ハム":   "f",
+}
+
 func (s *baseBallApiServer) Crawler(ctx context.Context, in *emptypb.Empty) (*emptypb.Empty, error) {
-	// s.logger.Info(ctx, "start crawler")
-	// // ページ番号を指定して、URLを構築する
-	// url := fmt.Sprintf("https://baseball-data.com/stats/hitter-s/tpa-1.html")
-	// print(url)
+	s.logger.Info(ctx, "start crawler")
+	// ページ番号を指定して、URLを構築する
 
-	// // HTTP GETリクエストを送信し、レスポンスを取得する
-	// res, err := http.Get(url)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer res.Body.Close()
+	teamPlayersMap := map[string][]domain.CreatePlayerParam{}
+	for teamName, siteUID := range teamSiteUIDMap {
+		fmt.Printf("\033[31m%#v\033[0m\n", teamName)
+		url := fmt.Sprintf("https://baseball-data.com/stats/hitter-%v/tpa-1.html", siteUID)
+		res, err := http.Get(url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
 
-	// // レスポンスのHTMLを解析する
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	// fmt.Printf("\033[31m%#v\033[0m\n", res.Body)
+		params := []domain.CreatePlayerParam{}
+		doc.Find("#tbl tbody tr").Each(func(_ int, s *goquery.Selection) {
+			param := domain.CreatePlayerParam{
+				UniformNumber: cast.StringToInt64(s.Find("td").Eq(0).Text()),
+				Name:          strings.ReplaceAll(s.Find("td").Eq(1).Text(), "\u3000", ""),
+				AtBats:        cast.StringToInt64(s.Find("td").Eq(5).Text()),
+				Hits:          cast.StringToInt64(s.Find("td").Eq(6).Text()),
+				Walks:         cast.StringToInt64(s.Find("td").Eq(10).Text()),
+				HomeRuns:      cast.StringToInt64(s.Find("td").Eq(7).Text()),
+				RunsBattedIn:  cast.StringToInt64(s.Find("td").Eq(8).Text()),
+			}
+			params = append(params, param)
+		})
+		teamPlayersMap[teamName] = params
+	}
 
-	// doc, err := goquery.NewDocumentFromReader(res.Body)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// // テーブルの各行を処理する
-	// doc.Find("table.data > tbody > tr").Each(func(i int, s *goquery.Selection) {
-	// 	// 各列の値を取得する
-	// 	rank := s.Find("td:nth-child(1)").Text()
-	// 	name := s.Find("td:nth-child(2)").Text()
-	// 	team := s.Find("td:nth-child(3)").Text()
-	// 	games := s.Find("td:nth-child(4)").Text()
-	// 	paStr := s.Find("td:nth-child(5)").Text()
-	// 	pa, err := strconv.Atoi(strings.ReplaceAll(paStr, ",", ""))
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-
-	// 	// 取得した値を出力する
-	// 	fmt.Printf("%s: %s (%s) - %d games, %d PA\n", rank, name, team, games, pa)
-	// })
-
+	fmt.Printf("\033[31m%#v\033[0m\n", teamPlayersMap)
 	return &emptypb.Empty{}, nil
 }

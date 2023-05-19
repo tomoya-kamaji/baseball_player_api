67c8172d96fce7b8fe87ffa418cabd2f055e4aaf
tomoya <tomoya.kamaji@gmail.com>
2023-05-19 12:20:26 +0900

    微修正

--- src/internal/player/request.go
+++ src/internal/player/request.go
@@ -5,6 +5,7 @@ type createPlayerRequest struct {
 	Name          string `json:"name" binding:"required" format:"string" example:"田中太郎"`
 	AtBats        int64  `json:"at_bats" binding:"required" format:"int64" example:"100"`
 	Hits          int64  `json:"hits" binding:"required" format:"int64" example:"30"`
-	Walks         int64  `json:"walks" binding:"required" format:"int64" example:"10" `
-	HomeRuns      int64  `json:"home_runs" binding:"required"format:"int64" example:"5" `
+	Walks         int64  `json:"walks" binding:"required" format:"int64" example:"10"`
+	HomeRuns      int64  `json:"home_runs" binding:"required" format:"int64" example:"5"`
+	RunsBattedIn  int64  `json:"runs_batted_in" binding:"required" format:"int64" example:"20"`
 }

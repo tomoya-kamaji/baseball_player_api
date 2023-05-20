4a709e192b4ec9f61797cca74baa95e4c707bb90
tomoya <tomoya.kamaji@gmail.com>
2023-05-20 18:04:06 +0900

    searchQueryの実装

--- src/query/playerSeachQuery.go
+++ src/query/playerSeachQuery.go
@@ -20,10 +20,10 @@ type PlayerSearchQuery interface {
 }
 
 type PlayerSearchQueryDTO struct {
-	Players []*Player
+	Players []*PlayerDTO
 }
 
-type Player struct {
+type PlayerDTO struct {
 	ID            string
 	UniformNumber int64
 	Name          string

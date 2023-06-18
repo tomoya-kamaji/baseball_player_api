fd09e1d0cef834f6c0bebedd72225480edd3251a
tomoya <tomoya.kamaji@gmail.com>
2023-06-18 17:47:58 +0900

    ヘルスチェック

--- baseballApi/src/route/v1/route.go
+++ baseballApi/src/route/v1/route.go
@@ -3,18 +3,17 @@ package v1
 import (
 	"github.com/gin-gonic/gin"
 	healthIn "github.com/tomoya_kamaji/go-pkg/src/internal/health"
-	playerIn "github.com/tomoya_kamaji/go-pkg/src/internal/player"
 )
 
 func Init(api *gin.Engine) {
 	r := api.Group("/v1")
+	r.GET("/health", healthIn.HealthCheck)
 
-	pl := r.Group("/players")
+	// pl := r.Group("/players")
 	{
-		r.GET("/health", healthIn.HealthCheck)
-		pl.GET("/search", playerIn.SearchPlayer)
-		pl.GET("/:id", playerIn.FetchPlayerById)
-		pl.POST("", playerIn.CreatePlayer)
-		pl.POST("/crawl", playerIn.Crawler)
+		// pl.GET("/search", playerIn.SearchPlayer)
+		// pl.GET("/:id", playerIn.FetchPlayerById)
+		// pl.POST("", playerIn.CreatePlayer)
+		// pl.POST("/crawl", playerIn.Crawler)
 	}
 }

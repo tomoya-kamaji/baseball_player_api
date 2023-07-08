568f766e34a7c37411925765fd696516cbe7114e
tomoya <tomoya.kamaji@gmail.com>
2023-07-08 22:22:42 +0900

    makeファイルの整理

--- baseballApi/db_migrate/schema.sql
+++ baseballApi/db_migrate/schema.sql
@@ -1,5 +1,26 @@
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    publisher VARCHAR(255),
+    publication_year INT,
+    genre VARCHAR(255)
+);
+
 CREATE TABLE users (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
-    status VARCHAR(255)
+    address VARCHAR(255),
+    contact_number VARCHAR(255)
+);
+
+CREATE TABLE loans (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT,
+    user_id INT,
+    loan_date DATE,
+    due_date DATE,
+    return_date DATE,
+    CONSTRAINT fk_books_loans FOREIGN KEY (book_id) REFERENCES books (id),
+    CONSTRAINT fk_users_loans FOREIGN KEY (user_id) REFERENCES users (id)
 );
